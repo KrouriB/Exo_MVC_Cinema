8fd1dc4e5683b6897cab5fa38b79d6f28d3b7949
KrouriB <brice.krouri@gmail.com>
2023-03-16 14:15:20 +0100

    template a utilisé sur les page a afficher

---
+++ view/template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title><?= $titre ?></title>
+</head>
+
+<body>
+    <nav class="topbar">
+
+    </nav>
+    <div id="wrapper">
+        <main>
+            <div id="contenu">
+                <h1>PDO Cinema</h1>
+                <h2><?= $titre_secondaire ?></h2>
+                <?= $content ?>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
