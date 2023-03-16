a08db44047bdd2e7ac3995321a28ef56e86e7b9e
KrouriB <brice.krouri@gmail.com>
2023-03-16 16:58:21 +0100

    ajout differente requete prevue+modif requete list

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -8,8 +8,10 @@ class CinemaController {
     public function listFilms() {
         $pdo = Connect::seConnecter();
         $requete = $pdo->query("
-            SELECT titre_film, date_sortie_film 
-            FROM film
+            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), "%H:%i") AS duree , CONCAT(p.nom_personne," ",p.prenom_personne) AS real
+            FROM film f
+            INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
+            INNER JOIN personne p ON r.id_personne = p.id_personne
         ");
         require "view/listFilms.php";
     }
@@ -22,7 +24,30 @@ class CinemaController {
             WHERE id_acteur = :id
         ");
         $requete->execute(["id"=>$id]);
-        require "view/acteur/detailActeur.php";
+        require "view/detailActeur.php";
     }
+
+    public function detReal($id){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT * 
+            FROM realisateur 
+            WHERE id_realisateur = :id
+        ");
+        $requete->execute(["id"=>$id]);
+        require "view/detailRealisaeur.php";
+    }
+
+    public function detFilm($id){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT * 
+            FROM film 
+            WHERE id_film = :id
+        ");
+        $requete->execute(["id"=>$id]);
+        require "view/detailFilm.php";
+    }
+
 
 }
