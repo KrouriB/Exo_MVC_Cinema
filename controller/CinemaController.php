6cdae291d98fe1d452b9b575e5e40098e17027ef
KrouriB <brice.krouri@gmail.com>
2023-03-17 10:48:27 +0100

    modif listFilms et ajout des autre list

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -5,16 +5,59 @@ use Model\Connect;
 
 class CinemaController {
 
+    // commande list
+
     public function listFilms() {
         $pdo = Connect::seConnecter();
         $requete = $pdo->query("
-            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%H:%i') AS duree , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
+            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%Hh%i') AS duree , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
             FROM film f
             INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
             INNER JOIN personne p ON r.id_personne = p.id_personne
+            ORDER BY sortie
         ");
         require "view/listFilms.php";
     }
+
+    public function listActeurs(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomActeur ,a.sexe ,a.date_naissance_acteur AS date , COUNT(c.id_acteur) AS nbLa ,a.id_acteur AS id
+            FROM acteur a
+            INNER JOIN casting c ON a.id_acteur = c.id_acteur
+            INNER JOIN personne p ON a.id_personne = p.id_personne
+            GROUP BY a.id_acteur
+            ORDER BY nbLa DESC , date ASC
+        ");
+        require "view/listActeurs.php";
+    }
+
+    public function listReals(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+        SELECT CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal , COUNT(f.id_realisteur) AS nbLa ,r.id_realisteur AS id
+        FROM realisateur r
+        INNER JOIN film f ON r.id_realisteur = f.id_realisteur
+        INNER JOIN personne p ON r.id_personne = p.id_personne
+        GROUP BY r.id_realisteur
+        ORDER BY nbLa DESC 
+        ");
+        require "view/listReals.php";
+    }
+
+    public function listRoles(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT r.nom_role , COUNT(c.id_role) AS nbLa ,e.id_role AS id
+            FROM role r
+            INNER JOIN casting c ON r.id_role = c.id_role
+            GROUP BY r.id_role
+            ORDER BY nbLa DESC , r.nom_role ASC
+        ");
+        require "view/listRoles.php";
+    }
+
+    // commande detail
 
     public function detActeur($id){
         $pdo = Connect::seConnecter();
