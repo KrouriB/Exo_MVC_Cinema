879c5d756b40ca5980166671aaa6d38e62500d31
KrouriB <brice.krouri@gmail.com>
2023-03-17 15:30:38 +0100

    affichage des acteur dans quel film par role

--- view/detailRole.php
+++ view/detailRole.php
@@ -1,10 +1,15 @@
 <?php
 ob_start();
+$role = $requete->fetchAll();
 ?>
 
+<div class="liste"><?php foreach($role as $unRole){ ?>
+    <p>Jouer par <a href="index.php?action=detActeurs&id=<?= $unRole['id_acteur'] ?>"><?= $unRole['nomActeur'] ?></a> dans <a href="index.php?action=detFilms&id=<?= $unRole['id_film'] ?>"><?= $unRole['titre_film'] ?></a></p>
+<?php } ?></div>
+
 <?php
-$titre = "Le role ".$ ;
-$titre_secondaire = "";
+$titre = $role[0]['nom_role'] ;
+$titre_secondaire = "Le role ".$role[0]['nom_role'];
 $contenu = ob_get_clean();
 require "view/template.php";
 ?>
