53bec63b3ec030a223ad940831926a129efc377c
KrouriB <brice.krouri@gmail.com>
2023-03-17 10:06:54 +0100

    ajout de la navbar

--- view/template.php
+++ view/template.php
@@ -10,15 +10,22 @@
 </head>
 
 <body>
-    <nav class="topbar">
-
-    </nav>
+    <header>
+        <nav class="topbar">
+            <ul>
+                <li><a href="index.php?action=listFilms">Films</a></li>
+                <li><a href="index.php?action=listActeurs">Acteurs</a></li>
+                <li><a href="index.php?action=listReals">Réalisateurs</a></li>
+                <li><a href="index.php?action=listRoles">Rôles</a></li>
+            </ul>
+        </nav>
+    </header>
     <div id="wrapper">
         <main>
             <div id="contenu">
                 <h1>PDO Cinema</h1>
                 <h2><?= $titre_secondaire ?></h2>
-                <?= $content ?>
+                <?= $contenu ?>
             </div>
         </main>
     </div>
