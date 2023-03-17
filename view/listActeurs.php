092adcb560c9220e256bc6c08aac9281e528a4b9
KrouriB <brice.krouri@gmail.com>
2023-03-17 10:47:19 +0100

    ajout du tableau a afficher

--- view/listActeurs.php
+++ view/listActeurs.php
@@ -2,9 +2,30 @@
 ob_start();
 ?>
 
+<table>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Sexe</th>
+            <th>Date de Naissance</th>
+            <th>Nombre d'appartion</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($requete->fetchAll() as $acteur){ ?>
+                <tr>
+                    <td><a href="index.php?action=detActeur&id=<?= $acteur["id"] ?>"><?= $acteur["nomActeur"] ?></a></td>
+                    <td><?= $acteur["sexe"] ?></td>
+                    <td><?= $acteur["date"] ?></a></td>
+                    <td><?= $acteur["nbLa"] ?></td>
+                </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
 <?php
-$titre = "Liste des Acteurs";
-$titre_secondaire = "Liste des Acteurs";
+$titre = "Liste des acteurs";
+$titre_secondaire = "Liste des acteurs";
 $contenu = ob_get_clean();
 require "view/template.php";
 ?>
