965d8eeba1f733d16c35fda6c154acf16a71410d
KrouriB <brice.krouri@gmail.com>
2023-03-17 10:57:50 +0100

    ajout du tableau a afficher

--- view/listRealisateurs.php
+++ view/listRealisateurs.php
@@ -2,9 +2,26 @@
 ob_start();
 ?>
 
+<table>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Nombre de réalisation</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($requete->fetchAll() as $role){ ?>
+                <tr>
+                    <td><a href="index.php?action=detReal&id=<?= $role["id"] ?>"><?= $role["nomReal"] ?></a></td>
+                    <td><?= $role["nbLa"] ?></td>
+                </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
 <?php
-$titre = "Liste des Realisateurs";
-$titre_secondaire = "Liste des Realisteurs";
+$titre = "Liste des realisateurs";
+$titre_secondaire = "Liste des realisateurs";
 $contenu = ob_get_clean();
 require "view/template.php";
 ?>
