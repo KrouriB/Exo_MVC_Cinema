e1d25939ea5f28532af3246a0dfaaf77e4776543
KrouriB <brice.krouri@gmail.com>
2023-03-17 09:51:44 +0100

    ajouts des cases

--- index.php
+++ index.php
@@ -14,8 +14,11 @@ if(isset($_GET["action"])){
     switch ($_GET["action"]){
 
         case "listFilms" : $ctrlCinema->listFilms(); break;
+        case "listActeurs" : $ctrlCinema->listActeurs(); break;
+        case "listRoles" : $ctrlCinema->listRoles(); break;
         case "detActeurs" : $ctrlCinema->detActeurs(); break;
         case "detReal" : $ctrlCinema->detActeurs(); break;
         case "detFilm" : $ctrlCinema->detActeurs(); break;
+        case "detRole" : $ctrlCinema->detRoles(); break;
     }
 }
