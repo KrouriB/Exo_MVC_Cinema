6c555909e66cff23a5da9368e6ca7ea02e7732dd
KrouriB <brice.krouri@gmail.com>
2023-03-17 10:56:55 +0100

    correction fonction appeler

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ if(isset($_GET["action"])){
         case "listFilms" : $ctrlCinema->listFilms(); break;
         case "listActeurs" : $ctrlCinema->listActeurs(); break;
         case "listRoles" : $ctrlCinema->listRoles(); break;
-        case "listReals" : $ctrlCinema->listRoles(); break;
+        case "listReals" : $ctrlCinema->listReals(); break;
         case "detActeurs" : $ctrlCinema->detActeurs(); break;
-        case "detReal" : $ctrlCinema->detActeurs(); break;
-        case "detFilm" : $ctrlCinema->detActeurs(); break;
+        case "detReal" : $ctrlCinema->detReals(); break;
+        case "detFilm" : $ctrlCinema->detFilms(); break;
         case "detRole" : $ctrlCinema->detRoles(); break;
     }
 }else {
