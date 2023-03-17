35861b1a82b7eb680f7c54fdea4e23aaeb51edb8
KrouriB <brice.krouri@gmail.com>
2023-03-17 17:20:30 +0100

    mise a jour de la base de donnée

--- cinema_brice.sql
+++ cinema_brice.sql
@@ -14,19 +14,104 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la base pour cinema_brice
+CREATE DATABASE IF NOT EXISTS `cinema_brice` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinema_brice`;
+
+-- Listage de la structure de table cinema_brice. acteur
+CREATE TABLE IF NOT EXISTS `acteur` (
+  `id_acteur` int NOT NULL AUTO_INCREMENT,
+  `sexe` varchar(50) NOT NULL,
+  `date_naissance_acteur` date NOT NULL,
+  `id_personne` int NOT NULL,
+  PRIMARY KEY (`id_acteur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `acteur_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de table cinema_brice. casting
+CREATE TABLE IF NOT EXISTS `casting` (
+  `id_film` int NOT NULL,
+  `id_acteur` int NOT NULL,
+  `id_role` int NOT NULL,
+  PRIMARY KEY (`id_film`,`id_acteur`,`id_role`),
+  KEY `casting_ibfk_2` (`id_acteur`),
+  KEY `casting_ibfk_3` (`id_role`),
+  CONSTRAINT `casting_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `casting_ibfk_2` FOREIGN KEY (`id_acteur`) REFERENCES `acteur` (`id_acteur`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `casting_ibfk_3` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de table cinema_brice. film
+CREATE TABLE IF NOT EXISTS `film` (
+  `id_film` int NOT NULL AUTO_INCREMENT,
+  `titre_film` varchar(50) NOT NULL,
+  `date_sortie_film` date NOT NULL,
+  `temps_min_film` int NOT NULL DEFAULT '0',
+  `resume_film` text,
+  `note_film` int DEFAULT NULL,
+  `image_film` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT 'indisponible.jpg',
+  `id_realisateur` int NOT NULL,
+  PRIMARY KEY (`id_film`),
+  KEY `FK_FILM_REALISATEUR` (`id_realisateur`),
+  CONSTRAINT `FK_FILM_REALISATEUR` FOREIGN KEY (`id_realisateur`) REFERENCES `realisateur` (`id_realisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
+-- Listage de la structure de table cinema_brice. genre
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id_genre` int NOT NULL AUTO_INCREMENT,
+  `libelle_genre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_genre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
 -- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table cinema_brice. genrer
+CREATE TABLE IF NOT EXISTS `genrer` (
+  `id_film` int NOT NULL,
+  `id_genre` int NOT NULL,
+  PRIMARY KEY (`id_film`,`id_genre`),
+  KEY `genrer_ibfk_2` (`id_genre`),
+  CONSTRAINT `genrer_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `genrer_ibfk_2` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table cinema_brice. personne
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id_personne` int NOT NULL AUTO_INCREMENT,
+  `nom_personne` varchar(50) NOT NULL,
+  `prenom_personne` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table cinema_brice. realisateur
+CREATE TABLE IF NOT EXISTS `realisateur` (
+  `id_realisateur` int NOT NULL AUTO_INCREMENT,
+  `id_personne` int NOT NULL,
+  PRIMARY KEY (`id_realisateur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `realisateur_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table cinema_brice. role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `nom_role` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
