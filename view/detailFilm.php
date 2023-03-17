e72e3b5fc4a1246b90e0809c6f92681c31dd6f38
KrouriB <brice.krouri@gmail.com>
2023-03-17 17:15:07 +0100

    correction var dans hautFilm + affichage casting

--- view/detailFilm.php
+++ view/detailFilm.php
@@ -1,23 +1,23 @@
 <?php
 ob_start();
 $film = $requete->fetchAll();
-$affiche = ($requete[0]['image_film']) ? "<figure><img src='/www/img/".$requete[0]['image_film']."' alt=''></figure>" : null ;
-$resume = ($requete[0]['resume_film']) ? "<p>".$requete[0]['resume_film']."</p>" : null ;
-if($requete[0]['note_film'] != null){
+$affiche = ($film[0]['image_film']) ? "<figure><img src='./www/img/".$film[0]['image_film']."' alt=''></figure>" : null ;
+$resume = ($film[0]['resume_film']) ? "<p>".$film[0]['resume_film']."</p>" : null ;
+if($film[0]['note_film'] != null){
     $note = "<div id='laNote'>";
-    if($requete[0]['note_film'] == 5){
-        for($i=0;$i<$requete[0]['note_film'];$i++){
+    if($film[0]['note_film'] == 5){
+        for($i=0;$i<$film[0]['note_film'];$i++){
             $note .= "<i class='fa-solid fa-star'></i>";
         }
-    }elseif($requete[0]['note_film'] == 0){
-        for($i=0;$i<$requete[0]['note_film'];$i++){
+    }elseif($film[0]['note_film'] == 0){
+        for($i=0;$i<$film[0]['note_film'];$i++){
             $note .= "<i class='fa-regular fa-star'></i>";
         }
     }else{
-        for($i=0;$i<$requete[0]['note_film'];$i++){
+        for($i=0;$i<$film[0]['note_film'];$i++){
             $note .= "<i class='fa-solid fa-star'></i>";
         }
-        for($i=0;$i<5-$requete[0]['note_film'];$i++){
+        for($i=0;$i<5-$film[0]['note_film'];$i++){
             $note .= "<i class='fa-regular fa-star'></i>";
         }
     }
@@ -30,13 +30,13 @@ if($requete[0]['note_film'] != null){
     <?= $affiche ?>
     <div id="droiteFilm">
         <?= $note ?>
-        <p>Ce film a été réaliser par <a href=""></a> d'une durée de  est sortie le </p>
+        <p>Ce film a été réaliser par <a href="index.php?action=detReals&id=<?= $film[0]['id_realisateur'] ?>"><?= $film[0]['nomReal'] ?></a> d'une durée de <?= $film[0]['duree'] ?> est sortie le <?= $film[0]['laDate'] ?></p>
         <?= $resume ?>
     </div>
 </div>
 
 <div class="liste"><?php foreach($film as $unFilm){ ?>
-    <p><?= ?></p>
+    <p><a href="index.php?action=detActeurs&id=<?= $unFilm['id_acteur'] ?>"><?= $unFilm['nomActeur'] ?></a> jouant dans le role de <a href="index.php?action=detRoles&id=<?= $unFilm['id_role'] ?>"><?= $unFilm['nom_role'] ?></a></p>
 <?php } ?></div>
 
 <?php
