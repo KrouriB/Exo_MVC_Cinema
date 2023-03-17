bd04f1b3311c0e94d16d22078863db877a09afac
KrouriB <brice.krouri@gmail.com>
2023-03-17 14:37:41 +0100

    inversion nom prenom

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -10,7 +10,7 @@ class CinemaController {
     public function listFilms() {
         $pdo = Connect::seConnecter();
         $requete = $pdo->query("
-            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%Hh%i') AS duree , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
+            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%Hh%i') AS duree , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
             FROM film f
             INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
             INNER JOIN personne p ON r.id_personne = p.id_personne
@@ -22,7 +22,7 @@ class CinemaController {
     public function listActeurs(){
         $pdo = Connect::seConnecter();
         $requete = $pdo->query("
-            SELECT CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomActeur ,a.sexe ,a.date_naissance_acteur AS date , COUNT(c.id_acteur) AS nbLa ,a.id_acteur AS id
+            SELECT CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomActeur ,a.sexe ,a.date_naissance_acteur AS date , COUNT(c.id_acteur) AS nbLa ,a.id_acteur AS id
             FROM acteur a
             INNER JOIN casting c ON a.id_acteur = c.id_acteur
             INNER JOIN personne p ON a.id_personne = p.id_personne
@@ -35,7 +35,7 @@ class CinemaController {
     public function listReals(){
         $pdo = Connect::seConnecter();
         $requete = $pdo->query("
-        SELECT CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal , COUNT(f.id_realisateur) AS nbLa ,r.id_realisateur AS id
+        SELECT CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , COUNT(f.id_realisateur) AS nbLa ,r.id_realisateur AS id
         FROM realisateur r
         INNER JOIN film f ON r.id_realisateur = f.id_realisateur
         INNER JOIN personne p ON r.id_personne = p.id_personne
@@ -62,7 +62,7 @@ class CinemaController {
     public function detActeur($id){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
-            SELECT *  , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomActeur
+            SELECT *  , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomActeur
             FROM acteur a
             INNER JOIN casting c ON a.id_acteur = c.id_acteur
             INNER JOIN personne p ON a.id_personne = p.id_personne
@@ -77,7 +77,7 @@ class CinemaController {
     public function detReals($id){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
-            SELECT *  , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomReal
+            SELECT *  , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal
             FROM realisateur r
             INNER JOIN film f ON r.id_realisateur = f.id_realisateur
             INNER JOIN personne p ON r.id_personne = p.id_personne
@@ -90,7 +90,7 @@ class CinemaController {
     public function detFilms($id){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
-            SELECT * , CONCAT(pr.nom_personne,' ',pr.prenom_personne) AS nomReal , CONCAT(pa.nom_personne,' ',pa.prenom_personne) AS nomActeur
+            SELECT * , CONCAT(pr.prenom_personne,' ',pr.nom_personne) AS nomReal , CONCAT(pa.prenom_personne,' ',pa.nom_personne) AS nomActeur
             FROM film f
             INNER JOIN realisateur re ON f.id_realisateur = re.id_realisateur
             INNER JOIN personne pr ON re.id_personne = pr.id_personne
@@ -107,7 +107,7 @@ class CinemaController {
     public function detRoles($id){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
-            SELECT *  , CONCAT(p.nom_personne,' ',p.prenom_personne) AS nomActeur
+            SELECT *  , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomActeur
             FROM role r
             INNER JOIN casting c ON r.id_role = c.id_role
             INNER JOIN acteur a ON c.id_acteur = a.id_acteur
