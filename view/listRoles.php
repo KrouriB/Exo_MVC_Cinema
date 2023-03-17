643c1e387f7406f197c0c964388fd7d35ddc938a
KrouriB <brice.krouri@gmail.com>
2023-03-17 11:10:28 +0100

    affichage du tableau

--- view/listRoles.php
+++ view/listRoles.php
@@ -2,9 +2,26 @@
 ob_start();
 ?>
 
+<table>
+    <thead>
+        <tr>
+            <th>Nom du Role</th>
+            <th>Nombre d'apparition</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($requete->fetchAll() as $role){ ?>
+                <tr>
+                    <td><a href="index.php?action=detRoles&id=<?= $role["id"] ?>"><?= $role["nom_role"] ?></a></td>
+                    <td><?= $role["nbLa"] ?></td>
+                </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
 <?php
-$titre = "Liste des Roles";
-$titre_secondaire = "Liste des Roles";
+$titre = "Liste des roles";
+$titre_secondaire = "Liste des roles";
 $contenu = ob_get_clean();
 require "view/template.php";
 ?>
