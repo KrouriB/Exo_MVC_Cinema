0d56a9df203f29d09bc7f57eebdc49646e5d9b28
KrouriB <brice.krouri@gmail.com>
2023-03-22 11:57:54 +0100

    passage de commentaire a fonctionnel

--- index.php
+++ index.php
@@ -17,12 +17,12 @@ if(isset($_GET["action"])){
         case "listActeurs" : $ctrlCinema->listActeurs(); break;
         case "listRoles" : $ctrlCinema->listRoles(); break;
         case "listReals" : $ctrlCinema->listReals(); break;
-        // case "listGenres" : $ctrlCinema->listGenres(); break;
+        case "listGenres" : $ctrlCinema->listGenres(); break;
         case "detActeurs" : $ctrlCinema->detActeurs($_GET['id']); break;
         case "detReals" : $ctrlCinema->detReals($_GET['id']); break;
         case "detFilms" : $ctrlCinema->detFilms($_GET['id']); break;
         case "detRoles" : $ctrlCinema->detRoles($_GET['id']); break;
-        // case "detGenres" : $ctrlCinema->detGenres($_GET['id']); break;
+        case "detGenres" : $ctrlCinema->detGenres($_GET['id']); break;
     }
 }else {
     $ctrlCinema->listFilms();
