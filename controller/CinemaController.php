5edda6e225d6c0d964cc9109666e88f50852997e
KrouriB <brice.krouri@gmail.com>
2023-03-22 15:56:57 +0100

    réarengement du fichier pour mieux y naviguer

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -5,19 +5,36 @@ use Model\Connect;
 
 class CinemaController {
 
-    // commande list
 
-    public function listFilms() {
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->query("
-            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%Hh%i') AS duree , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
-            FROM film f
-            INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
-            INNER JOIN personne p ON r.id_personne = p.id_personne
-            ORDER BY sortie
-        ");
-        require "view/listFilms.php";
+
+    // Personne
+    
+
+    
+    public function formPersonne(){
+        require "view/formPersonne.php";
     }
+
+
+
+    public function formulairePersonne(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
+    }
+
+
+
+
+
+    // Acteur
+
+
+
+
 
     public function listActeurs(){
         $pdo = Connect::seConnecter();
@@ -32,43 +49,7 @@ class CinemaController {
         require "view/listActeurs.php";
     }
 
-    public function listReals(){
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->query("
-        SELECT CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , COUNT(f.id_realisateur) AS nbLa ,r.id_realisateur AS id
-        FROM realisateur r
-        INNER JOIN film f ON r.id_realisateur = f.id_realisateur
-        INNER JOIN personne p ON r.id_personne = p.id_personne
-        GROUP BY r.id_realisateur
-        ORDER BY nbLa DESC 
-        ");
-        require "view/listRealisateurs.php";
-    }
 
-    public function listRoles(){
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->query("
-            SELECT r.nom_role , COUNT(c.id_role) AS nbLa ,r.id_role AS id
-            FROM role r
-            INNER JOIN casting c ON r.id_role = c.id_role
-            GROUP BY r.id_role
-            ORDER BY nbLa DESC , r.nom_role ASC
-        ");
-        require "view/listRoles.php";
-    }
-
-    public function listGenres(){
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->query("
-            SELECT ge.libelle_genre , COUNT(ger.id_genre) AS nbLa  ,ger.id_genre AS unGenre
-            FROM genrer ger
-            INNER JOIN genre ge ON ger.id_genre = ge.id_genre
-            GROUP BY ge.id_genre
-        ");
-        require "view/listGenres.php";
-    }
-
-    // commande detail
 
     public function detActeurs($id){
         $pdo = Connect::seConnecter();
@@ -85,19 +66,50 @@ class CinemaController {
         require "view/detailActeur.php";
     }
 
-    public function detReals($id){
+
+
+    public function formActeur(){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
-            SELECT *  , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , DATE_FORMAT(f.date_sortie_film, '%e/%m/%Y' ) AS laDate
-            FROM realisateur r
-            INNER JOIN film f ON r.id_realisateur = f.id_realisateur
-            INNER JOIN personne p ON r.id_personne = p.id_personne
-            WHERE r.id_realisateur = :id
-            ORDER BY f.date_sortie_film
-        ");
-        $requete->execute(["id"=>$id]);
-        require "view/detailRealisateur.php";
+        
+        ")
+        require "view/formActeur.php";
     }
+
+
+
+    public function formulaireActeur(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
+    }
+
+
+
+
+
+    // Film
+
+
+
+
+
+    public function listFilms() {
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT f.titre_film AS titre , YEAR(f.date_sortie_film) AS sortie , TIME_FORMAT(SEC_TO_TIME(f.temps_min_film*60), '%Hh%i') AS duree , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , f.id_film AS unFilm , f.id_realisateur AS unReal
+            FROM film f
+            INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
+            INNER JOIN personne p ON r.id_personne = p.id_personne
+            ORDER BY sortie
+        ");
+        require "view/listFilms.php";
+    }
+
+
 
     public function detFilms($id){
         $pdo = Connect::seConnecter();
@@ -124,6 +136,170 @@ class CinemaController {
         require "view/detailFilm.php";
     }
 
+
+
+    public function formFilm(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+        
+        ")
+        require "view/formFilm.php";
+    }
+
+
+
+    public function formulaireFilm(){
+        $pdo = Connect::seConnecter();
+        $requete1 = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete1->execute();
+        $requete2 = $pdo->prepare("
+        INSERT INTO  ()
+        VALUE ()
+        ");
+        $requete2->execute();
+    }
+
+
+
+
+
+    // Genre
+
+
+
+
+
+    public function listGenres(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT ge.libelle_genre , COUNT(ger.id_genre) AS nbLa  ,ger.id_genre AS unGenre
+            FROM genrer ger
+            INNER JOIN genre ge ON ger.id_genre = ge.id_genre
+            GROUP BY ge.id_genre
+        ");
+        require "view/listGenres.php";
+    }
+
+
+
+    public function detGenres($id){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT * 
+            FROM genre ge
+            INNER JOIN genrer ger ON ge.id_genre = ger.id_genre
+            INNER JOIN film f ON ger.id_film = f.id_film
+            WHERE ge.id_genre = :id
+        ");
+        $requete->execute(["id"=>$id]);
+        require "view/detailGenre.php";
+    }
+
+
+
+    public function formGenre(){
+        require "view/formGenre.php";
+    }
+
+
+
+    public function formulaireGenre(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
+    }
+
+
+
+
+
+    // Realisateur
+
+
+
+
+
+    public function listReals(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+        SELECT CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , COUNT(f.id_realisateur) AS nbLa ,r.id_realisateur AS id
+        FROM realisateur r
+        INNER JOIN film f ON r.id_realisateur = f.id_realisateur
+        INNER JOIN personne p ON r.id_personne = p.id_personne
+        GROUP BY r.id_realisateur
+        ORDER BY nbLa DESC 
+        ");
+        require "view/listRealisateurs.php";
+    }
+
+
+
+    public function detReals($id){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT *  , CONCAT(p.prenom_personne,' ',p.nom_personne) AS nomReal , DATE_FORMAT(f.date_sortie_film, '%e/%m/%Y' ) AS laDate
+            FROM realisateur r
+            INNER JOIN film f ON r.id_realisateur = f.id_realisateur
+            INNER JOIN personne p ON r.id_personne = p.id_personne
+            WHERE r.id_realisateur = :id
+            ORDER BY f.date_sortie_film
+        ");
+        $requete->execute(["id"=>$id]);
+        require "view/detailRealisateur.php";
+    }
+
+
+
+    public function formRealisateur(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT
+            FROM
+        ")
+        require "view/formRealisateur.php";
+    }
+
+
+
+    public function formulaireRealisateur(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
+    }
+
+
+
+
+
+    // Role
+
+
+
+
+
+    public function listRoles(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT r.nom_role , COUNT(c.id_role) AS nbLa ,r.id_role AS id
+            FROM role r
+            INNER JOIN casting c ON r.id_role = c.id_role
+            GROUP BY r.id_role
+            ORDER BY nbLa DESC , r.nom_role ASC
+        ");
+        require "view/listRoles.php";
+    }
+
+
+
     public function detRoles($id){
         $pdo = Connect::seConnecter();
         $requete = $pdo->prepare("
@@ -139,51 +315,63 @@ class CinemaController {
         require "view/detailRole.php";
     }
 
-    public function detGenres($id){
-        $pdo = Connect::seConnecter();
-        $requete = $pdo->prepare("
-            SELECT * 
-            FROM genre ge
-            INNER JOIN genrer ger ON ge.id_genre = ger.id_genre
-            INNER JOIN film f ON ger.id_film = f.id_film
-            WHERE ge.id_genre = :id
-        ");
-        $requete->execute(["id"=>$id]);
-        require "view/detailGenre.php";
+
+
+    public function formRole(){
+        require "view/formRole.php";
     }
 
-    // Commande formulaire
+
+
+    public function formulaireRole(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
+    }
+    
+    
+    
+    
+    
+    // Commande uniquement formulaire
+
+
+
+
+
+    // hub formulaire
 
     public function formGroup(){
         require "view/formGroup.php";
     }
 
-    public function formActeur(){
-        require "view/formActeur.php";
-    }
 
+
+    // Casting
+    
+
+    
     public function formCasting(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+        
+        ")
         require "view/formCasting.php";
     }
 
-    public function formFilm(){
-        require "view/formFilm.php";
+
+
+    public function formulaireCasting(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            INSERT INTO  ()
+            VALUE ()
+        ");
+        $requete->execute();
     }
 
-    public function formGenre(){
-        require "view/formGenre.php";
-    }
-
-    public function formPersonne(){
-        require "view/formPersonne.php";
-    }
-
-    public function formRealisateur(){
-        require "view/formRealisateur.php";
-    }
-
-    public function formRole(){
-        require "view/formRole.php";
-    }
 
 }
