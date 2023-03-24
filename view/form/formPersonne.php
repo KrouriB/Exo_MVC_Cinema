cec77d3e49ea3d863ec8f2d5054ece2e3c28e662
KrouriB <brice.krouri@gmail.com>
2023-03-24 17:04:00 +0100

    modif agencement fiv pour flex-box

--- view/form/formPersonne.php
+++ view/form/formPersonne.php
@@ -1,23 +1,27 @@
 <?php
-    ob_start();
+ob_start();
 ?>
 
 <form action="index.php?action=addPersonne" method="post">
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" name="nom">
+    <div class="divForm">
+        <div>
+            <label for="nom">Nom&nbsp;:</label>
+            <input type="text" name="nom">
+        </div>
+        <div>
+            <label for="prenom">Prénom&nbsp;:</label>
+            <input type="text" name="prenom">
+        </div>
     </div>
-    <div>
-        <label for="prenom">Prénom :</label>
-        <input type="text" name="prenom">
+    <div class="divSubmit">
+        <input type="submit" value="Ajoutez" name="submitPersonne">
     </div>
-    <input type="submit" value="Ajoutez" name="submitPersonne">
 </form>
 
 
 <?php
-    $titre = "Formulaire Personne";
-    $titre_secondaire = "Ajoutez une Personne :";
-    $contenu = ob_get_clean();
-    require "view/template.php";
+$titre = "Formulaire Personne";
+$titre_secondaire = "Ajoutez une Personne :";
+$contenu = ob_get_clean();
+require "view/template.php";
 ?>
